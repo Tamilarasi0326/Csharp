3358654e1a48316256b0c82cef6515cf3dd73f92
Tamilarasi S <2318065@cognizant.com>
2024-11-27 17:33:19 +0530

    Add project files.

---
+++ c#/1Basic/1PrintName.cs
@@ -0,0 +1,21 @@
+﻿/*1. Write a C# Sharp program to print Hello and your name in a separate line.
+Expected Output :
+Hello: Alexandra Abramov*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    public class Program1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Alexandra Abramov");
+        }
+    }
+}
