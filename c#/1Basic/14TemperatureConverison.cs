7dd59e70f9995397b17a6a009a75531f7a30109b
Tamilarasi0326 <149997099+Tamilarasi0326@users.noreply.github.com>
2024-11-30 14:29:44 +0530

    Program 14
    14. Write a C# program to convert Celsius degrees to Kelvin and Fahrenheit.
    Test Data:
    Enter the amount of celsius: 30
    Expected Output:
    Kelvin = 303
    Fahrenheit = 86

---
+++ c#/1Basic/14TemperatureConverison.cs
@@ -0,0 +1,26 @@
+﻿//14.Write a C# program to convert Celsius degrees to Kelvin and Fahrenheit.
+//Test Data:
+//Enter the amount of celsius: 30
+//Expected Output:
+//Kelvin = 303
+//Fahrenheit = 86
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    public class program14 
+    { 
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter the amount of celsius: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kelvin = {0}", c + 273);
+            Console.WriteLine("Fahrenheit = {0}", c * 18 / 10 + 32);
+        } 
+    }
+}
